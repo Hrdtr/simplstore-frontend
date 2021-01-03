fc3e3f5e07ed7cbbc9ff6f1fb7c5466fc31b8ec1
Herdi Tr <hrdtr@cognitive.id>
2021-01-03 15:31:41 +0700

    Add login error message

--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -20,9 +20,10 @@
                   <input
                     v-model="login.username"
                     type="text"
-                    class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-gray-100 rounded-md text-sm focus:outline-none w-full"
+                    class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-gray-100 rounded-md focus:outline-none w-full"
                     placeholder="Username"
                     style="transition: all 0.15s ease 0s"
+                    required
                   />
                 </div>
                 <div class="relative w-full mb-3">
@@ -34,17 +35,43 @@
                   <input
                     v-model="login.password"
                     type="password"
-                    class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-gray-100 rounded-md text-sm focus:outline-none w-full"
+                    class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-gray-100 rounded-md focus:outline-none w-full"
                     placeholder="Password"
                     style="transition: all 0.15s ease 0s"
+                    required
                   />
                 </div>
                 <div class="text-center mt-6">
+                  <p v-if="error" class="text-red-600 mb-1 text-sm">
+                    {{ error.response.data.message }}
+                  </p>
                   <button
-                    class="bg-gray-900 text-white text-sm font-bold uppercase px-6 py-3 rounded-md hover:bg-black transition-color duration-200 outline-none focus:outline-none mr-1 mb-1 w-full"
+                    class="inline-flex items-center justify-center bg-gray-900 text-white text-sm font-bold uppercase px-6 py-3 rounded-md hover:bg-black transition-color duration-200 outline-none focus:outline-none mr-1 mb-1 w-full"
                     type="submit"
                     style="transition: all 0.15s ease 0s"
+                    :disabled="submitting"
                   >
+                    <svg
+                      v-if="submitting"
+                      class="animate-spin -ml-1 mr-2 h-5 w-5 text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        class="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="#ffffff"
+                        stroke-width="4"
+                      ></circle>
+                      <path
+                        class="opacity-75"
+                        fill="#ffffff"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
                     Sign In
                   </button>
                 </div>
@@ -79,17 +106,21 @@ export default {
         username: '',
         password: '',
       },
+      error: null,
+      submitting: false,
     }
   },
   methods: {
     async userLogin() {
+      this.submitting = true
       try {
         const response = await this.$auth.loginWith('local', {
           data: this.login,
         })
         console.log(response)
       } catch (err) {
-        console.log(err)
+        this.error = err
+        this.submitting = false
       }
     },
   },
